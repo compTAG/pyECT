04c681a8afaa034e8761d2f2c6c2cb20bafc5f91
Jack Ruder <contact@jackruder.xyz>
2025-03-26 20:52:58 -0700

    Add scripting

--- examples/mona_lisa_ex.py
+++ examples/mona_lisa_ex.py
@@ -10,7 +10,11 @@ from pyect import (
     sample_directions_2d,
 )
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+devices = (
+    [torch.device("cpu"), torch.device("cuda")]
+    if torch.cuda.is_available()
+    else [torch.device("cpu")]
+)
 
 # Get the directory where this script resides
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,16 +22,41 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Build the absolute path to the image file
 image_path = os.path.join(script_dir, "mona_lisa.jpg")
 
-mona_lisa = image_to_grayscale_tensor(image_path, device)
+for device in devices:
+    mona_lisa = image_to_grayscale_tensor(image_path, device)
 
-complex = weighted_freudenthal(mona_lisa)
+    complex = weighted_freudenthal(mona_lisa)
 
-directions = sample_directions_2d(5).to(device)
-num_heights = 1000
+    directions = sample_directions_2d(5).to(device)
+    num_heights = 1000
 
-wect = WECT(directions, num_heights)
+    wect = WECT(directions, num_heights).eval()
 
-print(wect.forward(list(complex.dimensions)))
+    start = timeit.default_timer()
+    uncompiled_output = wect.forward(list(complex.dimensions))
+    end = timeit.default_timer()
+    print("Uncompiled output: ", uncompiled_output)
+    print(f"Time on {device} uncompiled: {end - start}")
+
+    scripted_wect = torch.jit.script(wect)
+
+    start = timeit.default_timer()
+    scripted_output = scripted_wect.forward(list(complex.dimensions))
+    end = timeit.default_timer()
+    print("Scripted output: ", scripted_output)
+    print(f"Time on {device} scripted: {end - start}")
+
+    # traced_wect = torch.jit.trace(wect, (list(complex.dimensions),))
+    #
+    # start = timeit.default_timer()
+    # traced_output = traced_wect.forward(list(complex.dimensions))
+    # end = timeit.default_timer()
+    # print(f"Time on {device} traced: {end - start}")
+    #
+    # compiled_wect = torch.compile(wect, backend="tensorrt")
+    # compiled_output = compiled_wect.forward(list(complex.dimensions))
+    # end = timeit.default_timer()
+    # print(f"Time on {device} compiled: {end - start}")
 
 
 def image_wect(
