f154c05fdcb4be4c9cfd864cad0a297c9fd6262b
alexjmccleary <alex.mccleary@gmail.com>
2025-04-13 10:47:02 -0600

    Add support for 3D arrays to sublevelset_ecf

--- pyect/sublevelset_ecf.py
+++ pyect/sublevelset_ecf.py
@@ -25,31 +25,72 @@ def _cell_indices(cell_values, num_values):
     """
     return torch.ceil(cell_values * (num_values-1)).long()
 
-def image_cell_values(img_arr):
+def cell_values_2D(arr):
     """
-    Creates a cubical complex with a function on its cells from an image array.
+    Creates a cubical complex with a function on its cells from a 2D tensor.
     The structure of the cubical complex is ignored with only the function values on the cells
     being recorded.
 
     Args:
-        img_arr (torch.Tensor): A 2D tensor with values between 0 and 1.
+        arr (torch.Tensor): A 2D tensor.
 
     Returns:
         vertex_values (torch.Tensor): A 1D tensor containing the function values of each vertex.
         edge_values (torch.Tensor): A 1D tensor containing the function values of each edge.
         square_values (torch.Tensor): A 1D tensor containing the function values of each square.
     """
-    img_arr = img_arr.float()
+    arr = arr.float()
 
-    vertex_values = img_arr.reshape(-1)
+    vertex_values = arr.reshape(-1)
 
-    horizontal_edge_values = torch.maximum(img_arr[:, 1:], img_arr[:, :-1]).reshape(-1)
-    vertical_edge_values = torch.maximum(img_arr[1:, :], img_arr[:-1, :]).reshape(-1)
-    edge_values = torch.cat([horizontal_edge_values, vertical_edge_values], dim=0)
+    x_edge_values = torch.maximum(arr[1:, :], arr[:-1, :])
+    y_edge_values = torch.maximum(arr[:, 1:], arr[:, :-1])
+    edge_values = torch.cat([
+        x_edge_values.reshape(-1),
+        y_edge_values.reshape(-1)
+    ], dim=0)
 
-    # torch.maximum can only handle two tensors at once so the max is split into several steps.
-    squares_1 = torch.maximum(img_arr[1:, 1:], img_arr[1:, :-1])
-    squares_2 = torch.maximum(img_arr[:-1, 1:], img_arr[:-1, :-1])
-    square_values = torch.maximum(squares_1, squares_2).reshape(-1) 
+    square_values = torch.maximum(y_edge_values[1:, :], y_edge_values[:-1, :]).reshape(-1)
 
     return [vertex_values, edge_values, square_values]
+
+def cell_values_3D(arr):
+    """
+    Creates a cubical complex with a function on its cells from a 3D tensor.
+    The structure of the cubical complex is ignored with only the function values on the cells
+    being recorded.
+
+    Args:
+        arr (torch.Tensor): A 3D tensor.
+
+    Returns:
+        vertex_values (torch.Tensor): A 1D tensor containing the function values of each vertex.
+        edge_values (torch.Tensor): A 1D tensor containing the function values of each edge.
+        square_values (torch.Tensor): A 1D tensor containing the function values of each square.
+        cube_values (torch.Tensor): A 1D tensor containing the function values of each cube.
+    """
+    arr = arr.float()
+
+    vertex_values = arr.reshape(-1)
+
+    x_edge_values = torch.maximum(arr[1:, ...], arr[:-1, ...])
+    y_edge_values = torch.maximum(arr[:, 1:, :], arr[:, :-1, :])
+    z_edge_values = torch.maximum(arr[..., 1:], arr[..., :-1])
+    edge_values = torch.cat([
+        x_edge_values.reshape(-1),
+        y_edge_values.reshape(-1),
+        z_edge_values.reshape(-1)
+        ], dim=0)
+
+    x_square_values = torch.maximum(y_edge_values[..., 1:], y_edge_values[..., :-1])
+    y_square_values = torch.maximum(z_edge_values[1:, ...], z_edge_values[:-1, ...])
+    z_square_values = torch.maximum(y_edge_values[1:, ...], y_edge_values[:-1, ...])
+    square_values = torch.cat([
+        x_square_values.reshape(-1),
+        y_square_values.reshape(-1),
+        z_square_values.reshape(-1)
+    ], dim=0)
+
+    cube_values = torch.maximum(x_square_values[1:, ...], x_square_values[:-1, ...]).reshape(-1)
+
+    return [vertex_values, edge_values, square_values, cube_values]
