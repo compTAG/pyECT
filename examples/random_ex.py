181930bc5721430f8a2c4ed2141478ae3b5b1630
alexjmccleary <alex.mccleary@gmail.com>
2025-04-15 11:13:44 -0600

    Add testing for Apple GPUs

--- examples/random_ex.py
+++ examples/random_ex.py
@@ -8,33 +8,61 @@ from pyect import (
     sample_directions_2d,
 )
 
-devices = (
-    [torch.device("cpu"), torch.device("cuda")]
-    if torch.cuda.is_available()
-    else [torch.device("cpu")]
-)
+image_size = (300, 300)
+directions = sample_directions_2d(20)
+num_heights = 10000
 
-for device in devices:
-    arr = torch.rand([4000,4000],device=device)
+img_arr = torch.rand(image_size)
 
-    complex = weighted_freudenthal(arr)
+#### CPU timing
 
-    directions = sample_directions_2d(20).to(device)
-    num_heights = 1000
+device = torch.device("cpu")
+complex = weighted_freudenthal(img_arr, device=device)
+directions = directions.to(device)
 
-    ########################################################
-    ## Run the WECT with different compilation strategies ##
-    ########################################################
+## Unscripted
+wect = WECT(directions, num_heights).eval()
+# burn in
+for i in range(5):
+    wect.forward(list(complex.dimensions))
 
-    ## Uncompiled
+start = timeit.default_timer()
+uncompiled_output = wect.forward(list(complex.dimensions))
+end = timeit.default_timer()
+print("Uncompiled output: ", uncompiled_output)
+print(f"Time on {device} uncompiled: {end - start}")
+
+## Scripted
+scripted_wect = torch.jit.script(wect)
+# Burn in
+for i in range(5):
+    scripted_wect.forward(list(complex.dimensions))
+
+start = timeit.default_timer()
+scripted_output = scripted_wect.forward(list(complex.dimensions))
+end = timeit.default_timer()
+print("Scripted output: ", scripted_output)
+print(f"Time on {device} scripted: {end - start}")
+
+#### CUDA
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    complex = weighted_freudenthal(img_arr, device=device)
+    directions = directions.to(device)
+
+    ## Unscripted
     wect = WECT(directions, num_heights).eval()
-    # burn in
+    # Burn in
     for i in range(5):
         wect.forward(list(complex.dimensions))
 
+    torch.cuda.synchronize()
     start = timeit.default_timer()
     uncompiled_output = wect.forward(list(complex.dimensions))
+    torch.cuda.synchronize()
     end = timeit.default_timer()
+
     print("Uncompiled output: ", uncompiled_output)
     print(f"Time on {device} uncompiled: {end - start}")
 
@@ -43,20 +71,55 @@ for device in devices:
     # Burn in
     for i in range(5):
         scripted_wect.forward(list(complex.dimensions))
+    
+    torch.cuda.synchronize()
 
     start = timeit.default_timer()
+
     scripted_output = scripted_wect.forward(list(complex.dimensions))
+    torch.cuda.synchronize()
+
     end = timeit.default_timer()
+
     print("Scripted output: ", scripted_output)
     print(f"Time on {device} scripted: {end - start}")
 
-    # ## Traced
-    # traced_wect = torch.jit.trace(wect, (list(complex.dimensions),))
-    # # Burn in
-    # for i in range(5):
-    #     traced_wect.forward(list(complex.dimensions))
-    #
-    # start = timeit.default_timer()
-    # traced_output = traced_wect.forward(list(complex.dimensions))
-    # end = timeit.default_timer()
-    # print(f"Time on {device} traced: {end - start}")
+
+#### Apple
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    complex = weighted_freudenthal(img_arr, device=device)
+    directions = directions.to(device)
+
+    ## Unscripted
+    wect = WECT(directions, num_heights).eval()
+    # Burn in
+    for i in range(5):
+        wect.forward(list(complex.dimensions))
+        torch.mps.synchronize()
+
+    start = timeit.default_timer()
+    uncompiled_output = wect.forward(list(complex.dimensions))
+    torch.mps.synchronize()
+    end = timeit.default_timer()
+
+    print("Uncompiled output: ", uncompiled_output)
+    print(f"Time on {device} uncompiled: {end - start}")
+
+    ## Scripted
+    scripted_wect = torch.jit.script(wect)
+    # Burn in
+    for i in range(5):
+        scripted_wect.forward(list(complex.dimensions))
+        torch.mps.synchronize()
+
+    start = timeit.default_timer()
+
+    scripted_output = scripted_wect.forward(list(complex.dimensions))
+    torch.mps.synchronize()
+
+    end = timeit.default_timer()
+
+    print("Scripted output: ", scripted_output)
+    print(f"Time on {device} scripted: {end - start}")
