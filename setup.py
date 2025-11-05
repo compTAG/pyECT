328aa14cdaabb95c1f74c52d4163038d04a9d159
quistew <quistew@gmail.com>
2025-11-05 09:45:50 -0700

    Add dependencies to publish package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyect',
-    version='0.1.0',
+    version='0.1.3',
     author='Alex McCleary, Eli Quist, Jack Ruder, Jacob Sriraman',
     author_email='eli.quist@student.montana.edu',
     description='Generalized computation of the WECT using PyTorch.',
@@ -14,6 +14,8 @@ setup(
     install_requires=[
         'torch',
         'Pillow',
+        'numpy',
+        'torchvision'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
