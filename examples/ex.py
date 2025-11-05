91683aa7e674926330e177249c1d33df26f8c677
alexjmccleary <alex.mccleary@gmail.com>
2025-11-04 18:13:40 -0700

    Minor update to ex

--- examples/ex.py
+++ examples/ex.py
@@ -9,39 +9,53 @@ from pyect import (
     image_to_grayscale_tensor
 )
 
-# We'll use a randomized array rather than an image file.
-# To apply this to an image file, use the function image_to_grayscale_tensor.
-# The resulting tensor can then be used in place of img_arr.
+def main():
+    # First, choose a device.
+    # Leave one of the following three lines uncommented to pick that device (if you have it):
 
-img_arr = torch.rand((500,500))
+    device = torch.device("cpu")       # default option
+    #device = torch.device("cuda")     # if you have a Nvidia GPU
+    #device = torch.device("mps")      # if you have an Apple m-series chip
 
-# From here we can then take the image ECF.
-# We first initialize the Image_ECF_2D module.
-# Here we're choosing to sample the ECF over 100 points.
 
-ecf = Image_ECF_2D(100).eval()
+    # We'll use a randomized array rather than an image file.
+    # To apply this to an image file, use the function image_to_grayscale_tensor.
+    # The resulting tensor can then be used in place of img_arr.
 
-# Next, we compute the image ECF of img_arr.
+    img_arr = torch.rand((500,500), device=device)
 
-ecf_result = ecf.forward(img_arr)
-print(ecf_result)
+    # From here we can then take the image ECF.
+    # We first initialize the Image_ECF_2D module.
+    # Here we're choosing to sample the ECF over 6 bins.
 
-# We can also compute the WECT of img_arr.
-# To do so, we first sample direction vectors.
-# We'll pick 50 directions for this example.
+    ecf = Image_ECF_2D(6).eval()
 
-directions = sample_directions_2d(50)
+    # Next, we compute the image ECF of img_arr.
 
-# Next, we initialize the WECT module with the direction vectors we just sampled.
-# We're choosing to sample 100 height values here.
+    ecf_result = ecf(img_arr)
+    print(f"ECF: {ecf_result}")
 
-wect = WECT(directions, 100).eval()
 
-# Now we compute the weighted Freudenthal complex of img_arr.
 
-img_complex = weighted_freudenthal(img_arr)
+    # We can also compute the WECT of img_arr.
+    # To do so, we first sample direction vectors.
+    # We'll pick 5 directions for this example.
 
-# Finally, we compute the wect of the resulting simplicial complex.
+    directions = sample_directions_2d(5, device=device)
 
-wect_result = wect.forward(img_complex)
-print(wect_result)
+    # Next, we initialize the WECT module with the direction vectors we just sampled.
+    # We're choosing to sample 6 height values here.
+
+    wect = WECT(directions, 6).eval()
+
+    # Now we compute the weighted Freudenthal complex of img_arr.
+
+    img_complex = weighted_freudenthal(img_arr, device=device)
+
+    # Finally, we compute the wect of the resulting simplicial complex.
+
+    wect_result = wect(img_complex)
+    print(f"WECT: {wect_result}")
+
+if __name__ == "__main__":
+    main()
